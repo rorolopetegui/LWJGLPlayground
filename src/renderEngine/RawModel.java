01a29c23536590e0ab2f401ca72c1736e61a45bc
Rodrigo Lopetegui <rlopetegui@altimetrik.com>
2018-09-07 17:10:15 -0300

    -init

---
+++ src/renderEngine/RawModel.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package renderEngine;
+
+/**
+ *
+ * @author rlopetegui
+ */
+public class RawModel {
+    private int vaoID;
+    private int vertexCount;
+    
+    public RawModel(int vaoID, int vertexCount){
+        this.vaoID = vaoID;
+        this.vertexCount = vertexCount;
+    }
+
+    public int getVaoID() {
+        return vaoID;
+    }
+
+    public int getVertexCount() {
+        return vertexCount;
+    }
+    
+    
+}
---
+++ src/shaders/StaticShader.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shaders;
+
+/**
+ *
+ * @author rlopetegui
+ */
+public class StaticShader extends  ShaderProgram{
+
+    private static final String VERTEX_FILE = "src/shaders/vertexShader.txt";
+    private static final String FRAGMENT_FILE = "src/shaders/fragmentShader.txt";
+    
+    public StaticShader() {
+        super(VERTEX_FILE, FRAGMENT_FILE);
+    }
+
+    @Override
+    protected void bindAttributes() {
+        super.bindAttribute(0, "position");
+    }
+    
+}
